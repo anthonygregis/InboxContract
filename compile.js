4b8d07b96e6bf81df5eb1d4ae1b9fe0e20f0bf84
Anthony Gregis <anthony@thedevtron.com>
2021-04-26 07:25:58 -0500

    [Deploy] Adds basic deploy program to deploy inbox contract on rinkeby network

--- compile.js
+++ compile.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
-const { exit } = require('process');
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+const { exit } = require("process");
 
 const contractPath = path.resolve(__dirname, "contracts");
 const fileNames = fs.readdirSync(contractPath);
 
 const input = {
-  language: 'Solidity',
+  language: "Solidity",
   sources: fileNames.reduce((input, fileName) => {
     const filePath = path.resolve(contractPath, fileName);
     const source = fs.readFileSync(filePath, "utf8");
     return { ...input, [fileName]: { content: source } };
   }, {}),
   settings: {
-      outputSelection: {
-          '*': {
-              '*': [ '*' ]
-          }
-      }
-  }
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
 };
 
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-if(output.errors) {
-  output.errors.forEach(err => {
-    console.error(err.formattedMessage)
-  })
-  exit(1)
+if (output.errors) {
+  output.errors.forEach((err) => {
+    console.error(err.formattedMessage);
+  });
+  exit(1);
 } else {
-  var contract = output.contracts['Inbox.sol'].Inbox;
+  var contract = output.contracts["Inbox.sol"].Inbox;
   var bytecode = contract.evm.bytecode.object;
   var interface = contract.abi;
 
-  module.exports = {interface, bytecode};
+  module.exports = { interface, bytecode };
 }
